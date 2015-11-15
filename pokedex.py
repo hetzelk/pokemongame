fcd1635928a825af3e374e60f8d21ed5d0d74051
keithhetzel <keithh.8112@yahoo.com>
2015-11-15 01:59:29 -0600

    a fully functioning ai player game. with difficulties

--- pokedex.py
+++ pokedex.py
@@ -1,6 +1,18 @@
-difficulty1 = {1:{'Pokemon': 'Balbasaur', 'move1': 'Tackle', 'move2': 'Razor', 'move3': 'Heal', 'health':100},2:{'Pokemon': 'Charmander', 'move1': 'Scratch', 'move2': 'Ember', 'move3': 'Heal', 'health':100},3:{'Pokemon': 'Squirtle', 'move1': 'Tackle', 'move2': 'Bubble', 'move3': 'Heal', 'health':100}}
-difficulty2 = {1:{'Pokemon': 'Ivysaur', 'move1': 'Tackle', 'move2': 'Razor', 'move3': 'Heal', 'health':200},2:{'Pokemon': 'Charmeleon', 'move1': 'Scratch', 'move2': 'Ember', 'move3': 'Heal', 'health':200},3:{'Pokemon': 'Wartortle', 'move1': 'Tackle', 'move2': 'Bubble', 'move3': 'Heal', 'health':200}}
-difficulty3 = {1:{'Pokemon': 'Venusaur', 'move1': 'Tackle', 'move2': 'Razor', 'move3': 'Heal', 'health':300},2:{'Pokemon': 'Charizard', 'move1': 'Scratch', 'move2': 'Ember', 'move3': 'Heal', 'health':300},3:{'Pokemon': 'Blastoise', 'move1': 'Tackle', 'move2': 'Bubble', 'move3': 'Heal', 'health':300}}
+import random
+difficulty1 = {
+1:{'Pokemon': 'Balbasaur', 'move1name': 'Tackle', 'move2name': 'Razor', 'move3name': 'Heal', 'health':100, 'move1':random.randint(15,25),'move2':random.randint(5,35),'move3':random.randint(10,20)},
+2:{'Pokemon': 'Charmander', 'move1name': 'Scratch', 'move2name': 'Ember', 'move3name': 'Heal', 'health':100, 'move1':random.randint(15,25),'move2':random.randint(5,35),'move3':random.randint(10,20)},
+3:{'Pokemon': 'Squirtle', 'move1name': 'Tackle', 'move2name': 'Bubble', 'move3name': 'Heal', 'health':100, 'move1':random.randint(15,25),'move2':random.randint(5,35),'move3':random.randint(10,20)}}
+
+difficulty2 = {
+1:{'Pokemon': 'Ivysaur', 'move1name': 'Tackle', 'move2name': 'Razor', 'move3name': 'Heal', 'health':200, 'move1':random.randint(30,50),'move2':random.randint(10,70),'move3':random.randint(20,40)},
+2:{'Pokemon': 'Charmeleon', 'move1name': 'Scratch', 'move2name': 'Ember', 'move3name': 'Heal', 'health':200, 'move1':random.randint(30,50),'move2':random.randint(10,70),'move3':random.randint(20,240)},
+3:{'Pokemon': 'Wartortle', 'move1name': 'Tackle', 'move2name': 'Bubble', 'move3name': 'Heal', 'health':200, 'move1':random.randint(30,50),'move2':random.randint(10,70),'move3':random.randint(20,40)}}
+
+difficulty3 = {
+1:{'Pokemon': 'Venusaur', 'move1name': 'Tackle', 'move2name': 'Razor', 'move3name': 'Heal', 'health':300, 'move1':random.randint(40,75),'move2':random.randint(20,85),'move3':random.randint(30,60)},
+2:{'Pokemon': 'Charizard', 'move1name': 'Scratch', 'move2name': 'Ember', 'move3name': 'Heal', 'health':300, 'move1':random.randint(40,75),'move2':random.randint(20,85),'move3':random.randint(30,60)},
+3:{'Pokemon': 'Blastoise', 'move1name': 'Tackle', 'move2name': 'Bubble', 'move3name': 'Heal', 'health':300, 'move1':random.randint(40,75),'move2':random.randint(20,85),'move3':random.randint(30,60)}}
 
 
 
@@ -18,195 +30,67 @@ wartortle = {'Pokemon': 'Wartortle', 'move1': 'Tackle', 'move2': 'Bubble', 'move
 blastoise = {'Pokemon': 'Blastoise', 'move1': 'Tackle', 'move2': 'Bubble', 'move3': 'Heal', 'health':300}
 
 
+in battle messages
+
+ __________________________________________
+| RIVAL WILLIAM                            |
+| Bulbasaur   Lv5                          |
+|      HP 100/100                          |
+|                                          |
+|                                          |
+|                            KEITH         |
+|                           SQUIRTLE   Lv5 |
+|                               HP 100/100 |
+|                                          |
+| What will            |   FIGHT       BAG |    -very first
+| SQUIRTLE do?         |   POKeMON     RUN |   game screen
+|__________________________________________|
+
+----
+| RIVAL ____name
+| would like to battle!
+----
+| What will
+| SQUIRTLE do?
+----
+| [1] TACKLE   [2] TAIL WHIP  |PP    35/35       -in game screen
+| [3] HEAL                    | type/NORMAL
 
 
 
+-A critical hit!
 
 
-import time, os, random
-from pokedex import * 
-
-
-
-
-class Player1():
-    def __init__(self):
-        name = str(input("What is your name? "))
-        self.name = name
-        self.pokedict = self.oaklabchoice()
-        self.pokemon = self.getname()
-        self.health = self.gethealth()
-        self.move1 = self.getmove1()
-        self.move2 = self.getmove2()
-        self.move3 = self.getmove3()
-
-    def oaklabchoice(self):
-        print("[B]albasaur, Grass-Type")
-        print("[C]harmander, Fire-Type")
-        print("[S]quirtle, Water-Type")
-        pokechoice = str(input('B, C, or S. Make your choice. ')).upper()
-
-        if pokechoice == "B":
-            return balbasaur   
-        if pokechoice == "C":
-            return charmander
-        if pokechoice == "S":
-            return squirtle
-        elif pokechoice != "B" or "C" or "S":
-            print("Please try again, B, C, or S. ")
-            print("")
-            return self.oaklabchoice()
-
-    def getname(self):
-        pokemon = self.pokedict
-        return pokemon['Pokemon']
-
-    def gethealth(self):
-        pokemon = self.pokedict
-        return pokemon['health']
-
-    def getmove1(self):
-        pokemon = self.pokedict
-        return pokemon['move1']
-
-    def getmove2(self):
-        pokemon = self.pokedict
-        return pokemon['move2']
-
-    def getmove3(self):
-        pokemon = self.pokedict
-        return pokemon['move3']
-
-
-    def attack(self):
-        attacks = input("Select a move")
-
-
-
-class Player2():
-    def __init__(self):
-        name = ("CPU")
-        self.name = name
-        self.pokedict = self.oaklabchoice()
-        self.pokemon = self.getname()
-        self.health = self.gethealth()
-        self.move1 = self.getmove1()
-        self.move2 = self.getmove2()
-        self.move3 = self.getmove3()
-
-    def oaklabchoice(self):
-        cpuchoices = ['B', 'C', 'S']
-        pokechoice = random.choice(cpuchoices)
-
-        if pokechoice == "B":
-            return balbasaur   
-        if pokechoice == "C":
-            return charmander
-        if pokechoice == "S":
-            return squirtle
-        elif pokechoice != "B" or "C" or "S":
-            print("Please try again, B, C, or S. ")
-            print("")
-            return self.oaklabchoice()
-
-    def getname(self):
-        pokemon = self.pokedict
-        return pokemon['Pokemon']
-
-    def gethealth(self):
-        pokemon = self.pokedict
-        return pokemon['health']
-
-    def getmove1(self):
-        pokemon = self.pokedict
-        return pokemon['move1']
-
-    def getmove2(self):
-        pokemon = self.pokedict
-        return pokemon['move2']
-
-    def getmove3(self):
-        pokemon = self.pokedict
-        return pokemon['move3']
+WILD RATTATA
+fainted!
 
 
 
 
 
 
+print("                                .::;                            ")
+print("                              .;:**'                            ")
+print("                                                                ")
+print("  .:XHHHHk.              db.   .;;.     dH  MX                  ")
+print("oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN       ")
+print("QMMMMMb  'MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM  ")
+print("  `MMMM.  )M> :X!Hk. MMMM   XMM.o'  .  MMMMMMM X?XMMM MMM>!MMP  ")
+print("   'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `' MX MMXXMM   ")
+print("    ~MMMMM~ XMM. .XM XM`'MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP   ")
+print("     ?MMM>  YMMMMMM! MM   `?MMRb.    `'''   !L'MMMMM XM IMMM    ")
+print("      MMMX   'MMMM'  MM       ~%:           !Mh.''' dMI IMMP    ")
+print("      'MMM.                                             IMX     ")
+print("       ~M!M                                             IMP     ")
 
 
 
-def battle(Player1, Player2):
-    print("Opponent wants to battle!")
-    print("Opponent sent", player2.pokemon, "!")
-    while player1.health > 0 and player2.health > 0:
-        
-        player1.attack() ##############################################this is where I left off, the next thing to do was select a move out of a list.
-        if player2.health <= 0:
-            break
-        print(player2, "2You have 0 health!")
-
-        player2.attack()
-        if player1.health <= 0:
-            break
-        print(player1, "1You have 0 health!")
-
-
-
-    if player1.health > 0:
-        print(player1,", you defeated",player2,"!")
-    if player2.health > 0:
-        print(player2,", you were defeated",player1,"!")
-
-
-
-
-
-def showpokemonascii():
     print(" ____       _                              ")
     print("|  _ \ ___ | | _____ _ __ ___   ___  _ __  ")
     print("| |_) / _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \ ")
     print("|  __/ (_) |   <  __/ | | | | | (_) | | | |")
     print("|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_|")
     print("")
-
-def intro():
-    print("Welcome to the pokemon world.")
-    print("There are 3 difficulty levels.")
-    print("You can face the computer or a friend.")
-    print("You take turns with your opponent.")
-    print("You have 3 different moves you can choose from.")
-    print("")
-    print("")
-
-def main():
-    os.system("cls")
-    print(" ____       _                              ")
-    print("|  _ \ ___ | | _____ _ __ ___   ___  _ __  ")
-    print("| |_) / _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \ ")
-    print("|  __/ (_) |   <  __/ | | | | | (_) | | | |")
-    print("|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_|")
-    print("")
-    print("Name:",player1.name)
-    print("Pokemon:",player1.pokemon)
-    print("Health:",player1.health)
-    print("Name:",player2.name)
-    print("Pokemon:",player2.pokemon)
-    print("Health:",player2.health)
-    #print("Move 1:",player1.move1,"- Move 2:",player1.move2,"- Move 3:",player1.move3)
-    print("")
-
-    battle(Player1(), Player2())
-
-
-os.system("cls")
-showpokemonascii()
-intro()
-player1 = Player1()
-player2 = Player2()
-main()
-
 
 
 
--- pokemon.py
+++ pokemon.py
@@ -2,12 +2,29 @@ import time, os, random
 from pokedex import * 
 
 
+os.system("cls")
 
+def showpokemonascii():
+    print("                                ,.::;                            ")
+    print("                              .;****'                            ")
+    print("  .:XHHHHk.              db.   .;;.     dH  MX                  ")
+    print("oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN       ")
+    print("QMMMMMb  'MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM  ")
+    print("  `MMMM.  )M> :X!Hk. MMMM   XMM.o'  .  MMMMMMM X?XMMM MMM>!MMP  ")
+    print("   'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `' MX MMXXMM   ")
+    print("    ~MMMMM~ XMM. .XM XM`'MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP   ")
+    print("     ?MMM>  YMMMMMM! MM   `?MMRb.    `'''   !L'MMMMM XM IMMM    ")
+    print("      MMMX   'MMMM'  MM       ~%:           !Mh.''' dMI IMMP    ")
+    print("      'MMM.                                             IMX     ")
+    print("       ~M!M                                             IMP     ")
+    print("")
+
+showpokemonascii()
 
 def multiplayer():
-    print("Would you like to play against the computer or a friend?")
+    print("Would you like to play against the computer or a rival?")
     print("[1] - Vs. Computer")
-    print("[2] - Vs. Friend")
+    print("[2] - Vs. Rival")
     multiplayerchoice = int(input("Select: "))
     if multiplayerchoice == 1:
         return 1
@@ -49,12 +66,19 @@ def difficulty():
 multiplayer = multiplayer()
 difficulty = difficulty()
 
-class Player1():
+class Character:
+    def __init__(self, health):
+        self.battlehealth = 0
+
+    def attack(self, other):
+        raise NotImplementedError
+
+class Player(Character):
     def __init__(self, multiplayer, difficulty):
         self.playername = 0
         self.multiplayer = multiplayer
         self.difficulty = difficulty #difficulty1, 2, 3
-        self.pokemon = 0
+        self.pokemon = 0 #set this to another variable so the computer can't select the same pokemon as the player.
         self.name1 = 0
         self.name2 = 0
         self.name3 = 0
@@ -65,6 +89,9 @@ class Player1():
         self.move1 = 0
         self.move2 = 0
         self.move3 = 0
+        self.move1amount = 0
+        self.move2amount = 0
+        self.move3amount = 0
 
     def getplayername(self):
         self.playername = input("Please enter your name. ")
@@ -112,25 +139,22 @@ class Player1():
     def gethealth(self):
         pokemon = self.pokedict
         self.health = pokemon['health']
-
+        self.battlehealth = pokemon['health']
     def getmove1(self):
         pokemon = self.pokedict
-        self.move1 = pokemon['move1']
-
+        self.move1 = pokemon['move1name']
     def getmove2(self):
         pokemon = self.pokedict
-        self.move2 = pokemon['move2']
-
+        self.move2 = pokemon['move2name']
     def getmove3(self):
         pokemon = self.pokedict
-        self.move3 = pokemon['move3']
+        self.move3 = pokemon['move3name']
 
 
-    def attack(self):
-        moves = {'move1':random.randint(15,25),'move2':random.randint(5,35),'move3':random.randint(10,20)}
+    def attack(self, other):
+        pokemon = self.pokedict
         
-
-        print("Your current health is:", self.health)
+        print("Your current health is:", self.battlehealth)
         print("[1]", self.move1)
         print("[2]", self.move2)
         print("[3]", self.move3)
@@ -138,19 +162,31 @@ class Player1():
 
         if attacks == 1:
             print(self.playername, "used", self.move1)
-            self.move = self.move1
+            damage = pokemon['move1']
+            movename = pokemon['move1name']
+            other.battlehealth -= damage
+            print(self.playername,"attacked with",movename,"dealing",damage,"damage.")
+
         elif attacks == 2:
             print(self.playername, "used", self.move2)
-            self.move = self.move2
+            damage = pokemon['move2']
+            movename = pokemon['move2name']
+            other.battlehealth -= damage
+            print(self.playername,"attacked with",movename,"dealing",damage,"damage.")
+
         elif attacks == 3:
             print(self.playername, "used", self.move3)
-            self.move = self.move3
+            health = pokemon['move3']
+            movename = pokemon['move3name']
+            self.battlehealth += health
+            print(self.playername,"used",movename,"adding",health,"health.")
+
         else:
             print("Please enter a correct choice.")
             return self.attack()
 
 
-class Player2():
+class Enemy(Character):
     def __init__(self, multiplayer, difficulty):
         self.playername = 0
         self.multiplayer = multiplayer
@@ -166,6 +202,9 @@ class Player2():
         self.move1 = 0
         self.move2 = 0
         self.move3 = 0
+        self.move1amount = 0
+        self.move2amount = 0
+        self.move3amount = 0
 
     def getplayername(self):
         if multiplayer == 1:
@@ -211,37 +250,63 @@ class Player2():
         self.battlehealth = pokemon['health']
     def getmove1(self):
         pokemon = self.pokedict
-        self.move1 = pokemon['move1']
+        self.move1 = pokemon['move1name']
+        self.move1amount = pokemon['move1']
     def getmove2(self):
         pokemon = self.pokedict
-        self.move2 = pokemon['move2']
+        self.move2 = pokemon['move2name']
+        self.move2amount = pokemon['move2']
     def getmove3(self):
         pokemon = self.pokedict
-        self.move3 = pokemon['move3']
+        self.move3 = pokemon['move3name']
+        self.move3amount = pokemon['move3']
 
-    def attack(self):
-        moves = {'move1':random.randint(15,25),'move2':random.randint(5,35),'move3':random.randint(10,20)}
-        
 
-        print(self.playername, "current health is:", self.health)
+    def attack(self, other):
+        pokemon = self.pokedict
+        print(self.playername, "current health is:", self.battlehealth)
 
         attacks = random.randint(1,100)
-
-        healpercent = self.health * .20
+        healpercent = self.health * .25
 
         if self.battlehealth <= healpercent:
-            print(self.playername, "used", self.move3)
-            self.move = self.move3
+            if attacks >= 65:
+                print(self.playername, "used", self.move3)
+                health = pokemon['move3']
+                movename = pokemon['move3name']
+                self.battlehealth += health
+                print(self.playername,"used",movename,"adding",health,"health.")
+            elif attacks >= 25:
+                print(self.playername, "used", self.move2)
+                damage = pokemon['move2']
+                movename = pokemon['move2name']
+                other.battlehealth -= damage
+                print(self.playername,"attacked with",movename,"dealing",damage,"damage.")
+            elif attacks >= 1:
+                print(self.playername, "used", self.move1)
+                damage = pokemon['move1']
+                movename = pokemon['move1name']
+                other.battlehealth -= damage
+                print(self.playername,"attacked with",movename,"dealing",damage,"damage.")
         else:
             if attacks >= 85:
                 print(self.playername, "used", self.move3)
-                self.move = self.move3
+                health = pokemon['move3']
+                movename = pokemon['move3name']
+                self.battlehealth += health
+                print(self.playername,"used",movename,"adding",health,"health.")
             elif attacks >= 50:
                 print(self.playername, "used", self.move2)
-                self.move = self.move2
+                damage = pokemon['move2']
+                movename = pokemon['move2name']
+                other.battlehealth -= damage
+                print(self.playername,"attacked with",movename,"dealing",damage,"damage.")
             elif attacks >= 1:
                 print(self.playername, "used", self.move1)
-                self.move = self.move1
+                damage = pokemon['move1']
+                movename = pokemon['move1name']
+                other.battlehealth -= damage
+                print(self.playername,"attacked with",movename,"dealing",damage,"damage.")
 
 
 
@@ -251,45 +316,50 @@ class Player2():
 
 
 
-def battle(Player1, Player2):
-    print("Opponent wants to battle!")
-    print("Opponent sent", comp.pokemon, "!")
-    while player.battlehealth > 0 and comp.battlehealth > 0:
+def battle(player, enemy):
+    print("Rival wants to battle!")
+    print("Rival sent", enemy.pokemon, "!")
+
+
+    while player.battlehealth > 0 and enemy.battlehealth > 0:
+        time.sleep(2.0)
+        os.system("cls")
+        showpokemonascii()
         
         print("")
-        player.attack() ##############################################this is where I left off, the next thing to do was select a move out of a list.
-        if comp.battlehealth <= 0:
-            print(comp.playername, "2You have 0 health!")
+        #displaygba()
+        player.attack(enemy)
+        if enemy.battlehealth <= 0:
+            print(enemy.playername, "you have 0 health!")
             break
+            winner()
 
         print("")
-        comp.attack()
+        enemy.attack(player)
         if player.battlehealth <= 0:
-            print(player.playername, "1You have 0 health!")
+            print(player.playername, "you have 0 health!")
             break
+            winner()
+    
 
+
+
+def winner():
+    player = Player(multiplayer, difficulty)
+    enemy = Enemy(multiplayer, difficulty)
 
     if player.battlehealth > 0:
-        print(player.playername,", you were defeated by",comp.playername,"!")
-    if comp.battlehealth > 0:
-        print(comp.playername,", you were defeated by",player.playername,"!")
+        print(player.playername,"you were defeated by",enemy.playername,"!")
+        print("")
+    if enemy.battlehealth > 0:
+        print(enemy.playername,"you were defeated by",player.playername,"!")
+        print("")
 
-
-
-
-
-def showpokemonascii():
-    print(" ____       _                              ")
-    print("|  _ \ ___ | | _____ _ __ ___   ___  _ __  ")
-    print("| |_) / _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \ ")
-    print("|  __/ (_) |   <  __/ | | | | | (_) | | | |")
-    print("|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_|")
-    print("")
 
 def intro():
     print("Welcome to the pokemon world.")
     print("There are 3 difficulty levels.")
-    print("You can face the computer or a friend.")
+    print("You can face the computer or a rival.")
     print("You take turns with your opponent.")
     print("You have 3 different moves you can choose from.")
     print("")
@@ -297,55 +367,55 @@ def intro():
 
 
 
-def run():
+def main():
     os.system("cls")
-    print(" ____       _                              ")
-    print("|  _ \ ___ | | _____ _ __ ___   ___  _ __  ")
-    print("| |_) / _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \ ")
-    print("|  __/ (_) |   <  __/ | | | | | (_) | | | |")
-    print("|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_|")
-    print("")
-    print("")
+    showpokemonascii()
+    intro()
+
+    player = Player(multiplayer, difficulty)
+    enemy = Enemy(multiplayer, difficulty)
+
     player.getplayername()
     print("")
-    #print("player # choice",player.multiplayer, "this is the dict for difficulty",player.difficulty)
     print("")
-    player.getnamechoices()
-    player.pokemonchoice()
-    player.gethealth()
-    player.getmove1()
-    player.getmove2()
-    player.getmove3()
-    print("Starting Health: ",player.health)
-    print("Move 1:",player.move1,"- Move 2:",player.move2,"- Move 3:",player.move3)
+    player.getnamechoices(),player.pokemonchoice(),player.gethealth(),player.getmove1(),player.getmove2(),player.getmove3()
+    print("Starting Health: ",player.health, player.battlehealth)
+    print(player.playername,"Move 1:",player.move1,"- Move 2:",player.move2,"- Move 3:",player.move3)
     print("")
 
-    comp.getplayername()
-    comp.getnamechoices()
-    comp.pokemonchoice()
-    comp.gethealth()
-    comp.getmove1()
-    comp.getmove2()
-    comp.getmove3()
-    print("Computer starting Health: ",comp.health)
-    print("Com Move 1:",comp.move1,"- Com Move 2:",comp.move2,"- Com Move 3:",comp.move3)
+    enemy.getplayername(),enemy.getnamechoices(),enemy.pokemonchoice(),enemy.gethealth(),enemy.getmove1(),enemy.getmove2(),enemy.getmove3()
+    print("Computer starting Health: ",enemy.health)
+    print(enemy.playername,"Move 1:",enemy.move1,"- Move 2:",enemy.move2,"- Move 3:",enemy.move3)
     print("")
     print("")
     print("")
-    #player.attack()
-    #comp.attack()
+    battle(player, enemy)
+    winner()
+
+def displaygba():
+    player = Player(multiplayer, difficulty)
+    enemy = Enemy(multiplayer, difficulty)
+    print(" __________________________________________")
+    print("| RIVAL {0}                                |".format(enemy.playername))#rival name
+    print("| {0}   Lv5                                |".format(enemy.pokemon))#rival pokemon
+    print("|      HP {0}/{1}                          |".format(enemy.battlehealth, enemy.health))#hp of starting hp
+    print("|                                          |")
+    print("|                                          |")
+    print("|                            {0}         |".format(player.playername))#name
+    print("|                           {0}   Lv5 |".format(player.pokemon))#pokemon
+    print("|                               HP {0}/{1} |".format(player.battlehealth, player.health))#hp of starting hp
+    print("|                                          |")
+    print("| [1] {0}   [2] {1} | PP     35/35|".format(player.move1name, player.move2name))#move1 move2
+    print("| [3] {0}                   |  type/NORMAL|".format(player.move3name))#move3
+    print("|__________________________________________|")
 
 
 
-os.system("cls")
-showpokemonascii()
 
-#intro()
-player = Player1(multiplayer, difficulty)
-#player2 =Player()
-comp = Player2(multiplayer, difficulty)
-run()
+if __name__ == '__main__':
+    os.system("cls")
+    main()
 
-battle(Player1, Player2)
+
 
 
