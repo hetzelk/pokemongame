c53873bc98dab026b0b099f019e89589c139b7ff
keithhetzel <keithh.8112@yahoo.com>
2015-11-16 08:47:24 -0600

    I changed some of the character move names, now they match their power.

--- pokedex.py
+++ pokedex.py
@@ -1,15 +1,15 @@
 difficulty1 = {
-1:{'Pokemon': 'Balbasaur', 'move1name': 'Tackle', 'move2name': 'Razor', 'move3name': 'Heal', 'health':100, 'move11':15, 'move12':25,'move21':5, 'move22':35,'move31':10, 'move32':20},
+1:{'Pokemon': 'Balbasaur', 'move1name': 'Tackle', 'move2name': 'Vine Whip', 'move3name': 'Heal', 'health':100, 'move11':15, 'move12':25,'move21':5, 'move22':35,'move31':10, 'move32':20},
 2:{'Pokemon': 'Charmander', 'move1name': 'Scratch', 'move2name': 'Ember', 'move3name': 'Heal', 'health':100, 'move11':15, 'move12':25,'move21':5, 'move22':35,'move31':10, 'move32':20},
 3:{'Pokemon': 'Squirtle', 'move1name': 'Tackle', 'move2name': 'Bubble', 'move3name': 'Heal', 'health':100, 'move11':15, 'move12':25,'move21':5, 'move22':35,'move31':10, 'move32':20}}
 
 difficulty2 = {
-1:{'Pokemon': 'Ivysaur', 'move1name': 'Tackle', 'move2name': 'Razor', 'move3name': 'Heal', 'health':200, 'move11':30, 'move12':50,'move21':10, 'move22':70,'move31':20, 'move32':40},
-2:{'Pokemon': 'Charmeleon', 'move1name': 'Scratch', 'move2name': 'Ember', 'move3name': 'Heal', 'health':200, 'move11':30, 'move12':50,'move21':10, 'move22':70,'move31':20, 'move32':40},
-3:{'Pokemon': 'Wartortle', 'move1name': 'Tackle', 'move2name': 'Bubble', 'move3name': 'Heal', 'health':200, 'move11':30, 'move12':50,'move21':10, 'move22':70,'move31':20, 'move32':40}}
+1:{'Pokemon': 'Ivysaur', 'move1name': 'Vine Whip', 'move2name': 'Razor Leaf', 'move3name': 'Heal', 'health':200, 'move11':30, 'move12':50,'move21':10, 'move22':70,'move31':20, 'move32':40},
+2:{'Pokemon': 'Charmeleon', 'move1name': 'Ember', 'move2name': 'Flamethrower', 'move3name': 'Heal', 'health':200, 'move11':30, 'move12':50,'move21':10, 'move22':70,'move31':20, 'move32':40},
+3:{'Pokemon': 'Wartortle', 'move1name': 'Bubble', 'move2name': 'Water Gun', 'move3name': 'Heal', 'health':200, 'move11':30, 'move12':50,'move21':10, 'move22':70,'move31':20, 'move32':40}}
 
 difficulty3 = {
-1:{'Pokemon': 'Venusaur', 'move1name': 'Tackle', 'move2name': 'Razor', 'move3name': 'Heal', 'health':300, 'move11':40, 'move12':75,'move21':20, 'move22':85,'move31':30, 'move32':60},
-2:{'Pokemon': 'Charizard', 'move1name': 'Scratch', 'move2name': 'Ember', 'move3name': 'Heal', 'health':300, 'move11':40, 'move12':75,'move21':20, 'move22':85,'move31':30, 'move32':60},
-3:{'Pokemon': 'Blastoise', 'move1name': 'Tackle', 'move2name': 'Bubble', 'move3name': 'Heal', 'health':300, 'move11':40, 'move12':75,'move21':20, 'move22':85,'move31':30, 'move32':60}}
+1:{'Pokemon': 'Venusaur', 'move1name': 'Razor Leaf', 'move2name': 'Petal Blizzard', 'move3name': 'Heal', 'health':300, 'move11':40, 'move12':75,'move21':20, 'move22':85,'move31':30, 'move32':60},
+2:{'Pokemon': 'Charizard', 'move1name': 'Flamethrower', 'move2name': 'Flare Blitz', 'move3name': 'Heal', 'health':300, 'move11':40, 'move12':75,'move21':20, 'move22':85,'move31':30, 'move32':60},
+3:{'Pokemon': 'Blastoise', 'move1name': 'Water Gun', 'move2name': 'Hydro Pump', 'move3name': 'Heal', 'health':300, 'move11':40, 'move12':75,'move21':20, 'move22':85,'move31':30, 'move32':60}}
 
